5e849c79e743d1694382ecb858a428aeb2f92d2d
arsalansadiq <arsalan.sadiq.1996@gmail.com>
2018-11-11 03:04:51 -0500

    CSS arranged for the images

--- A2/assignment2.js
+++ A2/assignment2.js
@@ -13,3 +13,15 @@ function toggleThumbnails(){
   }
 }
 </script>
+
+
+// $('#select-form img').click(function() {
+//     // Set the form value
+//     $('#image-value').val($(this).attr('data-value'));
+//
+//     // Unhighlight all the images
+//     $('#select-form img').removeClass('highlighted');
+//
+//     // Highlight the newly selected image
+//     $(this).addClass('highlighted');
+// });
