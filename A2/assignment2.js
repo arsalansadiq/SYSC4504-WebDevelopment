45dd8cce2c666d31cc3e276867ce711a221c0837
arsalansadiq <arsalan.sadiq.1996@gmail.com>
2018-11-11 23:29:49 -0500

    Update

--- A2/assignment2.js
+++ A2/assignment2.js
@@ -1,20 +1,20 @@
-<script type="text/javascript">
-function showImage(image){
-  var mainImage = document.getElementById('mainImage');
-  var mainImage = document.querySelector('[id^="big"]').id;
-  mainImage.src = image;
-}
-function toggleThumbnails(){
-  var thumbnails = document.getElementById('thumbnails');
-  if(thumbnails.style.display == 'block'){
-    thumbnails.style.display = 'none';
-  } else {
-    thumbnails.style.display = 'block';
-  }
-}
-</script>
-
-
+// <script type="text/javascript">
+// function showImage(image){
+//   var mainImage = document.getElementById('mainImage');
+//   var mainImage = document.querySelector('[id^="big"]').id;
+//   mainImage.src = image;
+// }
+// function toggleThumbnails(){
+//   var thumbnails = document.getElementById('thumbnails');
+//   if(thumbnails.style.display == 'block'){
+//     thumbnails.style.display = 'none';
+//   } else {
+//     thumbnails.style.display = 'block';
+//   }
+// }
+// </script>
+//
+//
 // $('#select-form img').click(function() {
 //     // Set the form value
 //     $('#image-value').val($(this).attr('data-value'));
