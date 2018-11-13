7579dcceb3f2c309812b3fd2838ec8beca9f6516
arsalansadiq <arsalan.sadiq.1996@gmail.com>
2018-11-13 17:57:12 -0500

    Update: Completed Assignment

--- A2/assignment2.js
+++ A2/assignment2.js
@@ -1,19 +1,21 @@
-$(window).load(function () {
+$(window).load(function () {//functions clickable when the page loads.
 
-    $("img[id^='small']").on('click', function () {
-        $newSrc = $(this).attr('src').replace("small","medium");
-        $("img[id*=big]").attr('src', $newSrc);
+    $("img[id^='small']").on('click', function () { //whenever any small pics are clicked
+        //change the image
+        $newSrc = $(this).attr('src').replace("small", "medium");//take the source of the item that's clicked and replace the word small in it to medium and save in variable $newSrc
+        $("img[id*=big]").attr('src', $newSrc);//change the src of the big image with the src replaced and called as $newSrc
 
-        $imageTitle = $(this).attr('title');
-        $("img[id*=big]").attr('title', $imageTitle);
-        $("img[id*=big]").next('figcaption').text($imageTitle);
+        //change the title and figcaption of big image when small pic is clicked
+        $imageTitle = $(this).attr('title');//take the title of the attribute thats clicked into the variable $imageTitle
+        $("img[id*=big]").attr('title', $imageTitle);//change the title of the big image with the $imageTitle
+        $("img[id*=big]").next('figcaption').text($imageTitle);//change the figcaption of the big image with the title $imageTitle
     });
 
-    //mouseover and mouseout stuff
-    $("img[id*=big],.text-block").mouseover(function () {
-        $('h2').text($("img[id*=big]").attr('title'));
-         $('.text-block').css('visibility','visible');
-    }).mouseout(function () {
-        $('.text-block').css('visibility','hidden');
+    //Display/overlap the image/div with mouseover and mouseout
+    $("img[id*=big],.overlayDiv").mouseover(function () {//whenver mouse is hovered on to the big image or the div of the small image
+        $('h2').text($("img[id*=big]").attr('title'));//change the h2 tag text of the overlayDiv with the title of the image 
+        $('.overlayDiv').css('visibility', 'visible');//make the OverlayDiv visible when mouse is hovered on
+    }).mouseout(function () {//whenver mouse is hovered out to the big image or the div of the small image
+        $('.overlayDiv').css('visibility', 'hidden');//make the OverlayDiv hidden when mouse is hovered out of the image
     });
 });
