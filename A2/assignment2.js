4ed051acdceda7823eb8d5dffc538b5deb78827d
Arsalan Sadiq <arsalansadiq@AD.DOE.CARLETON.CA>
2018-11-13 16:15:16 -0500

    Cleaned code and everything works

--- A2/assignment2.js
+++ A2/assignment2.js
@@ -1,31 +1,19 @@
-String.prototype.filename = function (extension) {
-    var s = this.replace(/\\/g, '/');
-    s = s.substring(s.lastIndexOf('/') + 1);
-    return extension ? s.replace(/[?#].+$/, '') : s.split('.')[0];
-}
-
 $(window).load(function () {
 
     $("img[id^='small']").on('click', function () {
-        $imgsrc = $(this).attr('src').filename();
-        $imgsrc = $imgsrc + '.jpg';
-
-        // alert(imgsrc);
-        $imagefolder = "images/medium/" + $imgsrc;
-        
-        $("img[id*=big]").attr('src', $imagefolder);
+        $newSrc = $(this).attr('src').replace("small","medium");
+        $("img[id*=big]").attr('src', $newSrc);
 
         $imageTitle = $(this).attr('title');
         $("img[id*=big]").attr('title', $imageTitle);
         $("img[id*=big]").next('figcaption').text($imageTitle);
-        // alert(imageTitle);
     });
 
     //mouseover and mouseout stuff
     $("img[id*=big],.text-block").mouseover(function () {
         $('h2').text($("img[id*=big]").attr('title'));
-         $('.text-block').show();
+         $('.text-block').css('visibility','visible');
     }).mouseout(function () {
-        $('.text-block').hide();
+        $('.text-block').css('visibility','hidden');
     });
 });
