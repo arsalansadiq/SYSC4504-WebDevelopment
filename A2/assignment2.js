f06561d6fcc670d30f8ab7eff6976267e9872f09
arsalansadiq <arsalan.sadiq.1996@gmail.com>
2018-11-13 12:54:05 -0500

    need to Replace the name of the div

--- A2/assignment2.js
+++ A2/assignment2.js
@@ -1,30 +1,33 @@
-String.prototype.filename=function(extension){
-    var s= this.replace(/\\/g, '/');
-    s= s.substring(s.lastIndexOf('/')+ 1);
-    return extension? s.replace(/[?#].+$/, ''): s.split('.')[0];
+String.prototype.filename = function (extension) {
+    var s = this.replace(/\\/g, '/');
+    s = s.substring(s.lastIndexOf('/') + 1);
+    return extension ? s.replace(/[?#].+$/, '') : s.split('.')[0];
 }
 
-$(window).load(function() {
+$(window).load(function () {
 
-    $("img[id^='small']").on('click',function(){
-        var imgsrc=$(this).attr('src').filename();
-        imgsrc = imgsrc+'.jpg';
+    $("img[id^='small']").on('click', function () {
+        $imgsrc = $(this).attr('src').filename();
+        $imgsrc = $imgsrc + '.jpg';
 
         // alert(imgsrc);
-       var imagefolder = "images/medium/"+imgsrc ; 
-        $("img[id*=big]").attr('src', imagefolder);
-        imageTitle = $(this).attr('title');
+        $imagefolder = "images/medium/" + $imgsrc;
+        $("img[id*=big]").attr('src', $imagefolder);
+        $imageTitle = $(this).attr('title');
 
-        $("img[id*=big]").attr('title', imageTitle);
-        $("img[id*=big]").next('figcaption').text(imageTitle);
+        $("img[id*=big]").attr('title', $imageTitle);
+        $("img[id*=big]").next('figcaption').text($imageTitle);
         // alert(imageTitle);
-});
+    });
     // alert("window load occurred!");
 
     //mouseover and mouseout stuff
     $("img[id*=big]").mouseover(function () {
-        $('.textblock').show();
+        $imageTitle = $("img[id*=big]").attr('title');
+        $newtitle = $('.text-block').text().replace($imageTitle);
+        // alert($newtitle);
+         $('.text-block').show();
     }).mouseout(function () {
-        $('.textblock').hide();
+        $('.text-block').hide();
     });
 });
