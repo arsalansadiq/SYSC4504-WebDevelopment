19e669097bbf468e4196c038cbfc03945f4907b6
arsalansadiq <arsalan.sadiq.1996@gmail.com>
2018-11-13 00:08:16 -0500

    Added the text block, now need to size it and implement the mouseover and out functionality

--- A2/assignment2.js
+++ A2/assignment2.js
@@ -1,31 +1,3 @@
-// <script type="text/javascript">
-// function showImage(image){
-//   var mainImage = document.getElementById('mainImage');
-//   var mainImage = document.querySelector('[id^="big"]').id;
-//   mainImage.src = image;
-// }
-// function toggleThumbnails(){
-//   var thumbnails = document.getElementById('thumbnails');
-//   if(thumbnails.style.display == 'block'){
-//     thumbnails.style.display = 'none';
-//   } else {
-//     thumbnails.style.display = 'block';
-//   }
-// }
-// </script>
-//
-//
-// $('#select-form img').click(function() {
-//     // Set the form value
-//     $('#image-value').val($(this).attr('data-value'));
-//
-//     // Unhighlight all the images
-//     $('#select-form img').removeClass('highlighted');
-//
-//     // Highlight the newly selected image
-//     $(this).addClass('highlighted');
-// });
-
 String.prototype.filename=function(extension){
     var s= this.replace(/\\/g, '/');
     s= s.substring(s.lastIndexOf('/')+ 1);
@@ -48,4 +20,11 @@ $(window).load(function() {
         // alert(imageTitle);
 });
     // alert("window load occurred!");
+
+    //mouseover and mouseout stuff
+    // $('.wrap').mouseover(function () {
+    //     $('.company-image-overlay').show();
+    // }).mouseout(function () {
+    //     $('.company-image-overlay').hide();
+    // });
 });
