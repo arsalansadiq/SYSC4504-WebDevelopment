d8139d24cd8e63103d02a94ea482b04ad931f71c
Arsalan <32456066+arsalansadiq@users.noreply.github.com>
2018-11-14 10:48:11 -0500

    Update: Changed the Figcaption last minute so removed div #AssignmentComplete

--- A2/assignment2.js
+++ A2/assignment2.js
@@ -8,12 +8,12 @@ $(window).load(function () {//functions clickable when the page loads.
         //change the title and figcaption of big image when small pic is clicked
         $imageTitle = $(this).attr('title');//take the title of the attribute thats clicked into the variable $imageTitle
         $("img[id*=big]").attr('title', $imageTitle);//change the title of the big image with the $imageTitle
-        $("img[id*=big]").next('figcaption').text($imageTitle);//change the figcaption of the big image with the title $imageTitle
+        $(".overlayDiv").children('h2').text($imageTitle);//change the children of figcaption h2 of the big image with the title $imageTitle
     });
 
     //Display/overlap the image/div with mouseover and mouseout
     $("img[id*=big],.overlayDiv").mouseover(function () {//whenver mouse is hovered on to the big image or the div of the small image
-        $('h2').text($("img[id*=big]").attr('title'));//change the h2 tag text of the overlayDiv with the title of the image 
+        $('.overlayDiv .h2').text($("img[id*=big]").attr('title'));//change the h2 tag text of the overlayDiv with the title of the image
         $('.overlayDiv').css('visibility', 'visible');//make the OverlayDiv visible when mouse is hovered on
     }).mouseout(function () {//whenver mouse is hovered out to the big image or the div of the small image
         $('.overlayDiv').css('visibility', 'hidden');//make the OverlayDiv hidden when mouse is hovered out of the image
