2db44a775b52c0e42e488d1f494b3b09dbb7a43f
Arsalan <32456066+arsalansadiq@users.noreply.github.com>
2018-11-30 15:42:45 -0500

    Added Shopping Cart File

---
+++ A3/Assignment3Start/ArtStore.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="utf-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Chapter 8</title>
+
+   <!-- Bootstrap core CSS -->
+   <link href="bootstrap3_defaultTheme/dist/css/bootstrap.css" rel="stylesheet">
+
+   <!-- Custom styles for this template -->
+   <link href="chapter08-project02.css" rel="stylesheet">
+
+   <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+   <!--[if lt IE 9]>
+   <script src="bootstrap3_defaultTheme/assets/js/html5shiv.js"></script>
+   <script src="bootstrap3_defaultTheme/assets/js/respond.min.js"></script>
+   <![endif]-->
+</head>
+
+<body>
+
+<?php include 'includes/art-header-inc.php';
+include 'art-data.php';
+$subtotal;
+$tax = 0.1;
+
+$grandtotal;
+
+function outputCartRow($file, $product, $quantity, $price) {
+
+  echo '<td><img class="img-thumbnail" src="images/art/tiny/' . $file.'"' . 'alt="..."></td>';
+  echo '<td>'.$product."</td>";
+  echo "<td>".$quantity."</td>";
+  echo "<td>".$price."</td>";
+  echo "<td>".($quantity*$price)."</td>";
+  global $subtotal;
+  $subtotal += ($quantity*$price);
+  global $grandtotal;
+  global $tax;
+  $grandtotal = $subtotal + ($subtotal*$tax);
+}
+?>
+
+
+<div class="container">
+
+      <div class="page-header">
+         <h2>View Cart</h2>
+      </div>
+
+      <table class="table table-condensed">
+         <tr>
+            <th>Image</th>
+            <th>Product</th>
+            <th>Quantity</th>
+            <th>Price</th>
+            <th>Amount</th>
+         </tr>
+         <tr>
+
+           <?php outputCartRow($file1, $product1, $quantity1, $price1); ?>
+
+         </tr>
+         <tr>
+
+           <?php outputCartRow($file2, $product2, $quantity2, $price2); ?>
+
+
+         </tr>
+         <tr class="success strong">
+           <?php
+          echo '<td colspan="4" class="moveRight">Subtotal</td>';
+          echo '<td >'.$subtotal.'</td>';
+            ?>
+         </tr>
+         <tr class="active strong">
+           <?php
+           echo '<td colspan="4" class="moveRight">Tax</td>';
+           echo '<td>'.$subtotal*$tax.'</td>';
+            ?>
+         </tr>
+         <tr class="strong">
+           <?php
+           $shipping = 100;
+           if($subtotal>2000){
+             $shipping=0;
+           }
+           echo '<td colspan="4" class="moveRight">Shipping</td>';
+           echo '<td>'.$shipping.'</td>' ?>;
+
+         </tr>
+         <tr class="warning strong text-danger">
+           <?php
+           echo '<td colspan="4" class="moveRight">Grand Total</td>';
+           echo '<td>'.number_format($grandtotal,2).'</td>';
+            ?>
+
+         </tr>
+         <tr >
+            <td colspan="4" class="moveRight"><button type="button" class="btn btn-primary" >Continue Shopping</button></td>
+            <td><button type="button" class="btn btn-success" >Checkout</button></td>
+         </tr>
+      </table>
+
+</div>  <!-- end container -->
+
+<?php include 'includes/art-footer-inc.php'; ?>
+
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="bootstrap3_defaultTheme/assets/js/jquery.js"></script>
+    <script src="bootstrap3_defaultTheme/dist/js/bootstrap.min.js"></script>
+  </body>
+</html>
--- A3/Assignment3Start/includes/art-artist-works.inc.php
+++ A3/Assignment3Start/includes/art-artist-works.inc.php
@@ -1,26 +1,52 @@
-<?php 
+<?php
 
+$connection = mysqli_connect(DBHOST, DBUSER, DBPASS, DBNAME);
+if ( mysqli_connect_errno() ) {
+  die( mysqli_connect_error() );
+}
 
+$sql = "select * from artists NATURAL JOIN artworks where ArtistID=". $_GET['id'];
 
 ?>
 
-<h3>Art by  </h3>  
+<h3>Art by <?php
+$FirstName = $row['FirstName'];
+$LastName = $row['LastName'];
+if($FirstName==null){
+  $fullName = $LastName;
+}else{
+  $fullName = $FirstName .' '. $LastName;
+}
+$fullName;
+ ?> </h3>
 
 <div class="row">
+  <?php
+  if ($result = mysqli_query($connection, $sql)) {
+    // loop through the data
+    while($row = mysqli_fetch_assoc($result))
+    {
+      $filename = $row['ImageFileName'];
 
-
-   <div class="col-md-3">
-      <div class="thumbnail">
-         <img src="images/art/works/square-medium/???.jpg" title="" alt="" class="img-thumbnail img-responsive">
-         <div class="caption">
-            <a class="btn btn-primary btn-xs" href="display-art-work.php?id="><span class="glyphicon glyphicon-info-sign"></span> View</a>
-            <button type="button" class="btn btn-success btn-xs"><span class="glyphicon glyphicon-gift"></span> Wish</button>
-            <button type="button" class="btn btn-info btn-xs"><span class="glyphicon glyphicon-shopping-cart"></span> Cart</button>
+      echo '<div class="col-md-3">
+         <div class="thumbnail">
+            <img src="images/art/works/square-medium/'.$filename.'.jpg" title="" alt="" class="img-thumbnail img-responsive">
+            <div class="caption">
+               <a class="btn btn-primary btn-xs" href="#"><span class="glyphicon glyphicon-info-sign"></span> View</a>
+               <button type="button" class="btn btn-success btn-xs"><span class="glyphicon glyphicon-gift"></span> Wish</button>
+               <button type="button" class="btn btn-info btn-xs"><span class="glyphicon glyphicon-shopping-cart"></span> Cart</button>
+            </div>
          </div>
-      </div>                   
-   </div>
+      </div>
+  ';
+    }
+    // release the memory used by the result set
+    mysqli_free_result($result);
+
+  }
+  mysqli_close($connection);
+  ?>
 
 
- 
-            
+
 </div>  <!-- end artist's works row -->
